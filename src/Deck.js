fcab11076b81a1300bd4490c54d70b67e4b08e1c
Hari <hariprasath.periyasamy@gmail.com>
2017-09-28 14:53:08 -0400

    new swipe removing ball

---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {View, Animated} from 'react-native';
+
+class Deck extends from Component {
+  render(){
+    return (
+      <View />
+    )
+  }
+}
+
+export default Deck;
